5a74d2bcc3a69cc96c06a79e69f31ecfd2defbb4
Pratik Devkota <pratikdevkota82@gmail.com>
2021-04-13 13:20:05 +0545

    Static and Media files configuration

--- notesewa_project/settings.py
+++ notesewa_project/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# serving files uploaded by user (development only)
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
