78d1fc37abe994e0804d3e9acc2f5c7aebab9a25
Pratik Devkota <pratikdevkota82@gmail.com>
2021-04-13 13:19:23 +0545

    Changed Post model

--- posts/models.py
+++ posts/models.py
@@ -8,3 +8,10 @@ class Post(models.Model):
     """
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=200)
+
+    def __str__(self):
+        """
+        Returns a representation of a post object,
+        which's displayed in admin page
+        """
+        return self.title
