a8fa0679b13ecf3ca1d2788e37d150ad17ed7559
Pratik Devkota <pratikdevkota82@gmail.com>
2021-04-13 12:52:02 +0545

    Fixed an error

--- notesewa_project/settings.py
+++ notesewa_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os.environ
+from os import environ as os_environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(DJANGO_SECRET_KEY)
+SECRET_KEY = os_environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
