6c67a8f3038abc48b271847c7cf9ca8a54b08362
Pratik Devkota <pratikdevkota82@gmail.com>
2021-04-20 21:08:36 +0545

    Add post detail views and templates

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -2,13 +2,18 @@
 from django.urls import path
 
 # app imports
-from .views import home
+from .views import home, PostDetailView
 
 
+app_name = 'posts'
 urlpatterns = [
         path(
             route = '',
             view  = home,
-            name  = 'posts_home'
-            ),
+            name  = 'posts_home'),
+
+        path(
+            route = 'posts/<slug:title_slug>/',
+            view  = PostDetailView.as_view(),
+            name  = 'posts_detail')
 ]
