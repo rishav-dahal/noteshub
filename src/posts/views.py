5b19f5f54cd7db1a10b386d3b27773ddba02f25b
Pratik Devkota <pratikdevkota82@gmail.com>
2021-04-23 23:30:18 +0545

    Using to function based views

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,5 @@
 # Core Django imports
-from django.shortcuts import render
-from django.views.generic import DetailView
+from django.shortcuts import render, get_object_or_404
 
 # app imports
 from .models import Post
@@ -14,15 +13,14 @@ def post_list(request):
     context = { 'recent_posts': Post.published.all().order_by('-created') }
     return render(request, template_name, context)
 
-class PostDetailView(DetailView):
+def post_detail(request, year, month, day, post_slug):
     """
-    Class based detail view for a single post
+    Function view for a single post
     """
-    model = Post
-    template_name = "posts/post_detail.html"
-
-    def get_object(self, queryset=None):
-        """
-        Return the object based on the kwargs given in the url
-        """
-        return Post.objects.get(title_slug=self.kwargs.get("title_slug"))
+    post = get_object_or_404(Post, title_slug=post_slug,
+                                   status='published',
+                                   created__year=year,
+                                   created__month=month,
+                                   created__day=day)
+    return render(request, template_name='posts/post_detail.html',\
+                           context={'post': post} )
